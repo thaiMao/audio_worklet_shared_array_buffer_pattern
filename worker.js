b6831035a3bb6f9eeef369b5eb676376fedf0931
thai <maot.tm@gmail.com>
2021-01-29 09:37:53 +0000

    initalise worker method

--- worker.js
+++ worker.js
@@ -70,14 +70,95 @@ let outputRingBuffer;
 /**
  * Process audio data in ring buffer
  */
-function processKernel() {}
+function processKernel() {
+  let inputReadIndex = states[STATE.IB_READ_INDEX];
+  let outputWriteIndex = states[STATE.OB_WRITE_INDEX];
+
+  // processing kernel that clones audio data sample by sample
+
+  for (let i = 0; i < WORKER_CONFIG.kernelLength; ++i) {
+    for (let j = 0; j < inputRingBuffer.length; ++j) {
+      outputRingBuffer[j][outputWriteIndex] =
+        inputRingBuffer[j][inputReadIndex];
+
+      if (outputWriteIndex++ === CONFIG.ringBufferLength) {
+        outputWriteIndex = 0;
+      }
+      if (inputReadIndex++ === CONFIG.ringBufferLength) {
+        inputReadIndex = 0;
+      }
+    }
+  }
+
+  states[STATE.IB_READ_INDEX] = inputReadIndex;
+  states[STATE.OB_WRITE_INDEX] = outputWriteIndex;
+}
 
 /**
  * Sleep until it's time to wake up, then process audio data to fill up outputRingBuffer
  */
-function waitOnRenderRequest() {}
+function waitOnRenderRequest() {
+  // As long as |REQUEST_RENDER| is zero, keep waiting. (sleep)
+  while (Atomics.wait(states, STATE.REQUEST_RENDER, 0) === "ok") {
+    processKernel();
+
+    // Update the number of available frames in the buffer.
+    states[STATE.IB_FRAMES_AVAILABLE] -= WORKER_CONFIG.kernelLength;
+    states[STATE.OB_FRAMES_AVAILABLE] += WORKER_CONFIG.kernelLength;
+
+    // Reset the request render bit, and wait again.
+    Atomics.store(states, STATE.REQUEST_RENDER, 0);
+  }
+}
 
 /**
  * Initialise Worker
  */
-function initialise() {}
+function initialise(options) {
+  const {
+    stateBufferLength,
+    bytesPerState,
+    ringBufferLength,
+    channelCount,
+    bytesPerSample,
+    kernelLength,
+  } = { ...WORKER_CONFIG, ...options };
+
+  if (!self.SharedArrayBuffer) {
+    postMessage({
+      message: "WORKER_ERROR",
+      detail: `SharedArrayBuffer is not supported in your browser. See
+            https://developers.google.com/web/updates/2018/06/audio-worklet-design-pattern
+            for more info.`,
+    });
+    return;
+  }
+
+  // Allocate SABs
+  const sharedBuffers = {
+    states: new SharedArrayBuffer(stateBufferLength * bytesPerState),
+    inputRingBuffer: new SharedArrayBuffer(
+      ringBufferLength * channelCount * bytesPerSample
+    ),
+    outputRingBuffer: new SharedArrayBuffer(
+      ringBufferLength * channelCount * bytesPerSample
+    ),
+  };
+
+  states = new Int32Array(sharedBuffers.states);
+  inputRingBuffer = [new Float32Array(sharedBuffers.inputRingBuffer)];
+  outputRingBuffer = [new Float32Array(sharedBuffers.outputRingBuffer)];
+
+  // Initalise states buffer
+  Atomics.store(states, STATE.RING_BUFFER_LENGTH, ringBufferLength);
+  Atomics.store(states, STATE.KERNEL_LENGTH, kernelLength);
+
+  // Notify AudioWorkletNode running in the main thread that Worker is ready
+  postMessage({
+    message: "WORKER_READY",
+    sharedBuffers,
+  });
+
+  // Start waiting
+  waitOnRenderRequest();
+}
