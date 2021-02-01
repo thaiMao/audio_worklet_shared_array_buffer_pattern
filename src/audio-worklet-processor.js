6ab995c6cf0e4c45411a3f4b5a7832b3eadc1dd3
thai <maot.tm@gmail.com>
2021-02-01 11:00:11 +0000

    keep connection open

--- src/audio-worklet-processor.js
+++ src/audio-worklet-processor.js
@@ -94,7 +94,7 @@ class SharedBufferWorkletProcessor extends AudioWorkletProcessor {
     const inputChannelData = inputs[0][0];
     const outputChannelData = outputs[0][0];
 
-    if (typeof inputChannelData === "undefined") return;
+    if (typeof inputChannelData === "undefined") return true;
 
     this._pushInputChannelData(inputChannelData);
     this._pullOutputChannelData(outputChannelData);
